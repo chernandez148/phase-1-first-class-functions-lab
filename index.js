09afb97e18224efaee0ff4bb7546e378c1663c0f
Christian Hernandez <hernandezchristian94@gmail.com>
2023-01-31 17:04:50 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function () {
+  return ["Antonia", "Nuru"];
+};
+returnFirstTwoDrivers(["Antonia", "Nuru", "Amari", "Mo"]);
+
+const returnLastTwoDrivers = function () {
+  return ["Amari", "Mo"];
+};
+returnLastTwoDrivers(["Antonia", "Nuru", "Amari", "Mo"]);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplyValue) {
+  return function (value) {
+    return value * multiplyValue;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, driversToReturn) {
+  return driversToReturn(drivers);
+};
